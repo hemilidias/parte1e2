add45f7ef9e35612ec1760650b92564fe6e54a03
Hemili Barbosa Dias <138241903+hemilidias@users.noreply.github.com>
2023-06-30 14:30:06 -0300

    Add files via upload

---
+++ JavaSimplesAtiv (1).java
@@ -0,0 +1,132 @@
+import java.sql.SQLOutput;
+
+public class JavaSimplesAtiv  { //Hemili Barbosa Dias 2-52 Atividades JAVA SIMPLES
+    public static void main(String[] args) {
+        System.out.println("\nAtividade 1");
+        System.out.println("Meu nome é Hemili");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 2");
+        System.out.println("A soma de 2 + 2 é igual a 4");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 3");
+        System.out.println("**********");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 4");
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 5");
+        System.out.println("        #                     ");
+        System.out.println("       ###                    ");
+        System.out.println("      #####                   ");
+        System.out.println("    #########                 ");
+        System.out.println("  #############               ");
+        System.out.println(" ###############              ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 6");
+        System.out.println("     .     ");
+        System.out.println("    ...    ");
+        System.out.println("   .....   ");
+        System.out.println("  .......  ");
+        System.out.println("   .....   ");
+        System.out.println("    ...    ");
+        System.out.println("     .     ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 7              ");
+        System.out.println("        $                  ");
+        System.out.println("       $$$                 ");
+        System.out.println("      $$$$$                ");
+        System.out.println("     $$$$$$$               ");
+        System.out.println("    $$$$$$$$$              ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 8");
+        System.out.println("        *                    ");
+        System.out.println("       ***                   ");
+        System.out.println("     *******                 ");
+        System.out.println("    *********                ");
+        System.out.println("  *************              ");
+        System.out.println("        *                    ");
+        System.out.println("        *                    ");
+        System.out.println("      *****                  ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 9");
+        System.out.println("       *****       *****       ");
+        System.out.println("     *        * *        *     ");
+        System.out.println("     *         *         *     ");
+        System.out.println("      *                 *      ");
+        System.out.println("        *             *        ");
+        System.out.println("           *       *           ");
+        System.out.println("               *               ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 10");
+        System.out.println("                   .....            ");
+        System.out.println("             ..            ..       ");
+        System.out.println("         ..                    ..   ");
+        System.out.println("        ..                      ..  ");
+        System.out.println("        ..           .          ..  ");
+        System.out.println("        ..           .          ..  ");
+        System.out.println("         ..          .         ..   ");
+        System.out.println("           ..        .        ..    ");
+        System.out.println("              ..     .     ..       ");
+        System.out.println("                   .....            ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 11");
+        System.out.println("                >              ");
+        System.out.println(" >                 >           ");
+        System.out.println(" > >>>>>>>>>>>>>>>    >        ");
+        System.out.println(" >                 >           ");
+        System.out.println("                >              ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 12");
+        System.out.println("         <                     ");
+        System.out.println("      <                 <      ");
+        System.out.println("   <  <<<<<<<<<<<<<<<<< <      ");
+        System.out.println("      <                 <      ");
+        System.out.println("         <                     ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 13");
+        System.out.println("              *                 ");
+        System.out.println("            *   *               ");
+        System.out.println("           *     *              ");
+        System.out.println("         *         *         ");
+        System.out.println("     *                 *      ");
+        System.out.println("         *         *            ");
+        System.out.println("           *     *              ");
+        System.out.println("            *   *            ");
+        System.out.println("              *              ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 14");
+        System.out.println(" # # # #");
+        System.out.println("# # # # ");
+        System.out.println(" # # # #");
+        System.out.println("# # # # ");
+        System.out.println(" # # # #");
+        System.out.println("# # # # ");
+        System.out.println(" # # # #");
+        System.out.println("# # # # ");
+        System.out.println("-------------------------------");
+        System.out.println("\nAtividade 15");
+        System.out.println(" *****************        ");
+        System.out.println("  ***************         ");
+        System.out.println("    ***********           ");
+        System.out.println("      *******             ");
+        System.out.println("       *****              ");
+        System.out.println("        ***               ");
+        System.out.println("         *                ");
+        System.out.println("-------------------------------");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
